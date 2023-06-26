dcbebae535c24df80c702cc3bd912171f3153279
Finbarr11 <61949899+Finbarr11@users.noreply.github.com>
2023-06-26 22:24:40 +0100

    update codes

---
+++ script.js
@@ -0,0 +1,34 @@
+// let myName = "Finbarr";
+// if (myName === "Finbarr") alert("Finbarr is a Frontend Engineer");
+
+// let myAge = 19;
+// console.log(myAge);
+
+// let myName = 'finbarrEshomo';
+// let Ages = 19 + 30 + 2 + 9;
+// console.log(Ages)
+
+
+//console.log(myName);
+
+
+let age = 30;
+let myName = "Finbarline";
+let isAnEngineer = true;
+let favFood;
+var school= "Auchi Polytechnic";
+var occupation = "Developer";
+
+console.log(age);
+console.log(myName);
+console.log( isAnEngineer);
+console.log(favFood);
+console.log(school);
+console.log(typeof occupation);
+
+
+const currentYear= 2023;
+const birthYear= 1993;
+const currentAge= currentYear - birthYear;
+
+console.log(currentAge);
